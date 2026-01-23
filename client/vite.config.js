eeb01f7bf1f8c1fd724214ddf02b352ff1406a31
Mallikarjun <mallikarjun.bm050@gmail.com>
2026-01-23 23:53:28 +0530

    paymentgateway changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://3.110.130.38:5000',
+                target: 'http://localhost:5000',
                 changeOrigin: true,
             },
         },
