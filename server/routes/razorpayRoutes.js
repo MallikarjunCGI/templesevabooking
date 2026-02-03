5c95a1c9e15109527db8b6e3de9b874b256a6a21
Mallikarjun <mallikarjun.bm050@gmail.com>
2026-02-03 10:39:58 +0530

    fix

--- server/routes/razorpayRoutes.js
+++ server/routes/razorpayRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createRazorpayOrder, createRazorpayPaymentLink } = require('../controllers/razorpayController');
-// Create Razorpay Payment Link (branded QR)
-router.post('/create-payment-link', createRazorpayPaymentLink);
+const { createRazorpayOrder } = require('../controllers/razorpayController');
 const { protect } = require('../middleware/authMiddleware');
 
 // Create Razorpay order (UPI only)
