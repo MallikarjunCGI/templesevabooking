4ee18c1c03e138ff60492d7696f68598fb23b5bf
Mallikarjun <mallikarjun.bm050@gmail.com>
2026-02-05 01:22:05 +0530

    fix

--- server/routes/razorpayRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createRazorpayOrder } = require('../controllers/razorpayController');
-const { protect } = require('../middleware/authMiddleware');
-
-// Create Razorpay order (UPI only)
-router.post('/create-order', createRazorpayOrder);
-
-module.exports = router;
